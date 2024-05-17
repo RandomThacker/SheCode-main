2aa5a801cc7c4aa752c43cfdf7e9b6e29aa2ec68
Brunda <brundaaishu@gmail.com>
2024-05-17 22:43:34 +0530

    Speakers section

--- index.js
+++ index.js
@@ -177,6 +177,33 @@ document.getElementById("track-prize-cards-container").onmousemove = (e) => {
     card.style.setProperty("--mouse-y", `${y}px`);
   }
 };
+// <!-- Speakers -->
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,    
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
 
 // <!-- Sponsors & Partners -->
 
